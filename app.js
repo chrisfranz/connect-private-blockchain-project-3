c72e63c75364baa3ae36291023831a1786908bd3
chrisfranz <chrisfranz@mac.com>
2019-03-15 09:18:15 -0700

    added comments to app.js

--- app.js
+++ app.js
@@ -34,16 +34,22 @@ app.get('/block/:height',
 
 app.post('/block', 
   async (req, res) => {
+    // destructure request body to get user-inputted data
     const { data } = req.body;
+    // if data field is empty, return error message
     if (!data) {
       res.status(400).send('invalid submission')
       return;
     }
     try {
+      // create new block with user-inputted data
       const newBlock = new Block.Block(data);
+      // add newBlock to db
       const block = await chain.addBlock(newBlock);
+      // return newly created block to client
       res.send(block);
     } catch(e) {
+      // if adding newBlock fails, return server error
       res.status(500).send('server error')
     }
 })
